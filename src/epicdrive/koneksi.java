82c672b60f22068801b91aefe8d786ff21ad5a2e
AmmarFarras <ammarfarras124@gmail.com>
2024-10-17 15:35:29 +0700

    first commit

---
+++ src/epicdrive/koneksi.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package epicdrive;
+
+/**
+ *
+ * @author Ammar
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+ 
+public class koneksi {
+    Connection con;
+    Statement stm;
+    
+    public void config(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost/epicdrive", "root", "");
+            stm = con.createStatement();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "koneksi gagal "+e.getMessage());
+        }
+    }
+}
